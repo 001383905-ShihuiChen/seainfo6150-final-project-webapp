575da463b02381fd491973eab83fe87594be27b4
cheryl28 <shihui@boonoob.com>
2020-12-10 23:32:13 -0800

    Updates

---
+++ precache-manifest.72d8d3fe54fd9eaed4166873a82c6287.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602fc520b20d917fd4bd3cbd62c2167c",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "fe66b6720a466df00335",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.0fd840cd.chunk.css"
+  },
+  {
+    "revision": "6b0267035fa77fa17b92",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.20698d09.chunk.js"
+  },
+  {
+    "revision": "fe66b6720a466df00335",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.3762c1f3.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "23fc5ec9aa641db15124733b19e37a55",
+    "url": "/seainfo6150-final-project-webapp/static/media/ThankImg.23fc5ec9.jpg"
+  },
+  {
+    "revision": "0f0a5d31bdaca0a35587dd0a4e8d73ad",
+    "url": "/seainfo6150-final-project-webapp/static/media/breakfastImg.0f0a5d31.jpg"
+  },
+  {
+    "revision": "78a3c0b16b96b909ad1f23d50341ab7c",
+    "url": "/seainfo6150-final-project-webapp/static/media/dessertImg.78a3c0b1.jpg"
+  },
+  {
+    "revision": "486bc4d33e06686b74728b2c6c0a700f",
+    "url": "/seainfo6150-final-project-webapp/static/media/dinnerImg.486bc4d3.jpeg"
+  },
+  {
+    "revision": "4175b563d3636f3940672994fd7f36da",
+    "url": "/seainfo6150-final-project-webapp/static/media/drinkImg.4175b563.jpg"
+  },
+  {
+    "revision": "8b0559531e081392428abc77d5f1371b",
+    "url": "/seainfo6150-final-project-webapp/static/media/homeImg.8b055953.jpg"
+  },
+  {
+    "revision": "fc50edbf280a8b1dca546ba019ffc189",
+    "url": "/seainfo6150-final-project-webapp/static/media/lunchImg.fc50edbf.jpg"
+  },
+  {
+    "revision": "bbe3380b0eba2a91139ca2b743557b8d",
+    "url": "/seainfo6150-final-project-webapp/static/media/navBarImg.bbe3380b.jpg"
+  },
+  {
+    "revision": "65d3b56d563e674df7b86bc2d0abc086",
+    "url": "/seainfo6150-final-project-webapp/static/media/noodleImg.65d3b56d.jpg"
+  },
+  {
+    "revision": "bbd3a7e67c76254d300a8b2eb171e422",
+    "url": "/seainfo6150-final-project-webapp/static/media/recipeImg.bbd3a7e6.png"
+  },
+  {
+    "revision": "90be50b9cc412e908a6a95c4879592e5",
+    "url": "/seainfo6150-final-project-webapp/static/media/sandwichImg.90be50b9.jpg"
+  },
+  {
+    "revision": "47b6f99033c0ff67810148b70844fbdc",
+    "url": "/seainfo6150-final-project-webapp/static/media/waffleImg.47b6f990.jpg"
+  }
+]);
